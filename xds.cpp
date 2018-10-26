5c67dbbb61393f9f4d63bfed6a0105549f599306
qyy2003 <qinyuyang2003@outlook.com>
2018-10-26 18:51:51 +0800

    new

--- xds.cpp
+++ xds.cpp
@@ -47,51 +47,58 @@ int free(int x){
     q.push(x);
     return free(son[x][0])+free(son[x][1]);
 }
-
+ 
 void read(int &x) {
     x = 0;
     char c = getchar();
     while (c < '0' || c > '9') c = getchar();
     while (c >= '0' && c <= '9')
-	x = (x << 3) + (x << 1) + c - '0', c = getchar();
+    x = (x << 3) + (x << 1) + c - '0', c = getchar();
 }
-
+ 
 int main(){
-    freopen("graph1.in","r",stdin);
-    freopen("graph.out","w",stdout);
+   // freopen("graph0.in","r",stdin);
+    //freopen("graph.out","w",stdout);
     read(n); read(m); read(l);
     for(int i=1;i<=n;i++) read(c[i]),sw[++nc]=c[i];
     sort(sw+1,sw+n+1);
     nc=unique(sw+1,sw+n+1)-sw-1;
-    for(int i=1;i<=nc;i++){lli[i]=1;
-	for(int &j=lli[i];sw[i]-sw[j]>=l;j++);}
-    for(int i=1;i<=nc;i++){llr[i]=nc;
-	for(int &j=llr[i];sw[j]-sw[i]>=l;j--);}
+    if(l){
+    for(int i=1,j=1;i<=nc;i++){
+    for(;sw[i]-sw[j]>=l;j++);
+    lli[i]=j;
+    }
+    for(int i=nc,j=nc;i>=1;i--){
+    for(;sw[j]-sw[i]>=l;j--);
+    llr[i]=j;
+    }
+    }
     for(int i=1;i<=n;i++) c[i]=lower_bound(sw+1,sw+nc+1,c[i])-sw;
     //for(int i=1;i<=n;i++) printf("%d %d %d\n",c[i],lli[c[i]],llr[c[i]]);
     for(int i=1;i<=m;i++){
-	read(x); read(y); read(z);
-	qq.push_back(make_pair(z,make_pair(x,y)));
+    read(x); read(y); read(z);
+    qq.push_back(make_pair(z,make_pair(x,y)));
     }
     sort(qq.begin(),qq.end());
-    for(int i=1;i<=n;i++) update(root[i],1,nc,c[i]),size[i]=1;
+    for(int i=1;i<=n;i++) update(root[i],1,nc,c[i]);
+    for(int i=1;i<=n;i++) size[i]=1;
     for(int i=1;i<=n;i++) fa[i]=i;
     for(int i=0;i<m;i++){
-	x=find(qq[i].second.first);
-	y=find(qq[i].second.second);
-	if(x==y) continue;
-	if(size[x]>size[y]) swap(x,y);
-	fa[x]=y; 
-	if(l==0){
-	    ans+=1ll*size[x]*size[y]*qq[i].first;
-	    size[y]+=size[x];
-	    continue;
-	}
-	dfs(root[x],1,nc,y,qq[i].first);
-	//printf(" %d %d\n",size[y],c[x]);
-	merge(root[y],root[x],1,nc);
-	free(root[x]);
-	size[y]+=size[x];
+    x=find(qq[i].second.first);
+    y=find(qq[i].second.second);
+    if(x==y) continue;
+    if(size[x]>size[y]) swap(x,y);
+    fa[x]=y;
+    if(l==0){
+        ans+=1ll*size[x]*size[y]*qq[i].first;
+        size[y]+=size[x];
+        continue;
+    }
+    dfs(root[x],1,nc,y,qq[i].first);
+    //printf(" %d %d\n",size[y],c[x]);
+    merge(root[y],root[x],1,nc);
+    free(root[x]);
+    size[y]+=size[x];
     }
     printf("%lld",ans);
 }
