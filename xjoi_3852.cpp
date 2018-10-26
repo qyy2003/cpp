dc2cfbb1d32edddb416865430e62a2e8dc8c3b6b
qyy2003 <qinyuyang2003@outlook.com>
2018-10-26 21:45:22 +0800

    new

--- xjoi_3852.cpp
+++ xjoi_3852.cpp
@@ -1,18 +1,28 @@
 #include<cstdio>
-#include<algorithm>
+#include<cmath>
 #include<iostream>
+#include<algorithm>
 using namespace std;
-int d1,d2,w1,w2,ma,maxa,mina,mb,maxb,minb,maxmin,minmax;
+double d1,d2,w1,w2;
+int mina,maxa,maxb,minb,x;
 int main(){
-    scanf("%d%d%d%d",&d1,&d2,&w1,&w2);
-//    if(w1<d1||w2<d2) return printf("Impossible"),0;
-    ma=d1/w1;mb=w2/d2;
-    mina=max(0,ma-w1/2);
-    maxa=2*ma-mina;
-    minb=max(0,mb-w2/2);
-    maxb=2*mb-minb;
-    maxmin=max(mina,minb);
-    minmax=min(maxa,maxb);
-    if(minmax>=maxmin) return printf("Possible"),0;
+    scanf("%lf%lf%lf%lf",&d1,&d2,&w1,&w2);
+    maxa=floor(w1/d1+d1/2.0)+1;mina=ceil(w1/d1-d1/2.0)-1;
+    if(1ll*d1*(d1+1.0)/2.0>=w1){
+	x=(w1*8.0+1.0);
+	maxa=floor((sqrt(x)-1.0)/2.0);
+	maxa++;
+	mina=-1;
+    }
+    maxb=floor(w2/d2+d2/2.0);minb=ceil(w2/d2-d2/2.0);
+    if(1ll*d2*(d2+1.0)/2.0>=w2){
+	x=(w2*8.0+1.0);
+	maxb=floor((sqrt(x)-1.0)/2.0);
+	//maxb=sqrt(w2*2.0);
+	minb=0;
+    }
+    printf("%d %d %d %d",maxa,mina,maxb,minb);
+    if(mina>maxb||maxa<minb)
     return printf("Impossible"),0;
+    return printf("Possible"),0;
 }
--- xjoi_3860.cpp
+++ xjoi_3860.cpp
@@ -1,15 +1,32 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+long long seed,ans;
+int n,m,a[800005],b[800005],f[800005];
+const int MAXN=3e5;
 int main(){
     scanf("%d%lld%d",&n,&seed,&m);
     for(int i=1;i<=n;i++){
 	a[i]=(seed/(1<<16))%m;
-	seed=(seed*1103515245+12345)%(1<<31);
+	a[i]++;
+	seed=(seed*1103515245+12345)%(1ll<<31);
+//	printf("%d ",a[i]);
     }
     for(int i=1;i<=m;i++){
 	memset(f,0,sizeof(f));
+	for(int k=MAXN;k<=MAXN*2;k+=k&(-k)) f[k]++;
 	for(int j=1;j<=n;j++){
-	    sum=0;
-	    for(int k=j;j>=0;j-=j&(-j)) sum+=f[j];
-	    ans+=sum;
-	    if(a[j]==m){
-		for(int k=j;j<=n;j+=(j&-j))
-
+	    if(a[j]==i) b[j]=b[j-1]+1;
+	    else b[j]=b[j-1];
+	    for(int k=2*b[j]-j+MAXN-1;k>0;k-=k&(-k)) ans+=f[k];
+	    //ans-=f[2*b[j]-j+MAXN-1];
+	    for(int k=2*b[j]-j+MAXN;k<=MAXN*2;k+=k&(-k)) f[k]++;
+	    //printf("%lld %d\n",ans,j);
+	}
+//	for(int j=1;j<=n;j++) printf("%d ",b[j]);
+  //  printf("%lld\n",ans);
+    }
+    printf("%lld",ans);
+}
